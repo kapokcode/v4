c9342e284a716a94f80054aaeca2efe10c6d6045
kapokcode <kapokoffical@gmail.com>
2023-03-13 22:36:12 +0800

    update about me

--- src/config.js
+++ src/config.js
@@ -4,23 +4,15 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://github.com/kapokcode',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/kapokcode',
     },
     {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      name: 'LeetCode',
+      url: 'https://leetcode.cn/u/kapokcode',
     },
   ],
 
