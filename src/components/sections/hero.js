4c16fd3c713563ba77ee667b6714906517bf3d9c
kapokcode <kapokoffical@gmail.com>
2023-03-13 22:31:21 +0800

    update about me

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -63,17 +63,10 @@ const Hero = () => {
   const two = <h2 className="big-heading">Kapok.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
-    <>
-      <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
-      </p>
-    </>
+    <p>
+      I specialize in creating web, and server applications that are not only efficient and scalable,
+      but also tailored to meet specific business needs.
+    </p>
   );
   const five = (
     <a
